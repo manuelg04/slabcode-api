bc69ec648db6ee6fb1246f4907e70d0e68044a1f
Manuel <manuelg9704@gmail.com>
2024-06-27 16:00:25 -0500

    test

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -11,7 +11,7 @@ export const createTask = async (req: Request, res: Response) => {
       data: {
         name,
         description,
-        projectId,
+        projectId,//hola
       },
     });
     res.json(task);
